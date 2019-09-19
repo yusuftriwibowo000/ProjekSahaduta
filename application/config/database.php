61ab417ed6b4e1456ba6d44f98600a020c567792
fanifadil <44403837+fanifadil@users.noreply.github.com>
2019-09-19 10:10:06 +0700

    asd

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'database' => 'db_sahaduta',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
---
+++ application/views/frontend/home/home.php
@@ -0,0 +1,25 @@
+<header id="gtco-header" class="gtco-cover gtco-cover-sm" role="banner" style="background-image: url(images/img_bg_3.jpg)" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="gtco-container">
+        <div class="row row-mt-15em">
+            <div class="col-md-7 mt-text text-left animate-box" data-animate-effect="fadeInUp">
+                <h1>Sahaduta <strong>Klinik </strong></h1>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="feature">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-area">
+                    <h2 class="title">Sahaduta Klinik</h2>
+                    <br>Klinik Sahaduta merupakan klinik yang bekerja di bidang kesehatan yang beralamat di tanggul,jember</br>
+                    <span class="line"></span>
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
